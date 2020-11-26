b2873528d3d44fa6e328fd6497b97672c1dc062e
testTestvvv <75091767+testTestvvv@users.noreply.github.com>
2020-11-26 21:13:13 +0300

    Add files via upload

---
+++ Task1/src/test/java/ru/stqa/maven/AppTest.java
@@ -0,0 +1,43 @@
+package ru.stqa.maven;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class AppTest 
+{
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement enterField = driver.findElement(By.name("q"));
+        enterField.sendKeys("webdriver");
+        enterField.sendKeys(Keys.RETURN);
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+}
